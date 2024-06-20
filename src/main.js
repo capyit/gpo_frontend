a714c6f934ab2a22329039b249fa72956df80d5b
De Sousa Ramalho, Filipe <privat@filiperamalho.de>
2024-06-20 16:59:23 +0200

    add yarn.lock

---
+++ src/main.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var m = require("mithril");
+var home_1 = require("./pages/home");
+var room_view_1 = require("./pages/room-view");
+var round_view_1 = require("./pages/round-view");
+var login_1 = require("./pages/login");
+var admin_1 = require("./pages/admin");
+var match_1 = require("./pages/match");
+var participant_1 = require("./pages/participant");
+var non_sign_1 = require("./pages/non-sign");
+var participants_1 = require("./pages/participants");
+require("../assets/css/main.css");
+var env_json_1 = require("../assets/env.json");
+require("normalize.css");
+var api_URL = env_json_1.default.api_url;
+var el = document.getElementById("content");
+m.route(el, "/home", {
+    "/home": home_1.default,
+    "/raeume": room_view_1.default,
+    "/runden": round_view_1.default,
+    "/participant/:id": participant_1.default,
+    "/non-sign": non_sign_1.default,
+    "/participants": participants_1.default,
+    "/login": login_1.default,
+    "/admin": admin_1.default,
+    "/match/:id": match_1.default,
+});
